74604105035a1f90856301e96a142b5f29677db9
Meekol <942049649@qq.com>
2019-05-28 17:33:39 +0800

    python版本的图像识别服务sdk迁移

---
+++ python/dark_enhance_token_demo.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+from image_sdk.gettoken import get_token
+from image_sdk.utils import encode_to_base64
+from image_sdk.utils import decode_to_wave_file
+from image_sdk.dark_enhance import dark_enhance
+from image_sdk.utils import init_global_env
+import json
+
+if __name__ == '__main__':
+    # Services currently support North China-Beijing 1 (cn-north-1), Asia Pacific-Hong Kong (ap-southeast-1)
+    init_global_env('cn-north-1')
+
+    #
+    # access image dark enhance by token
+    #
+    user_name = '*****'
+    password = '******'
+    account_name = '*****'  # the same as user_name in commonly use
+
+    token = get_token(user_name, password, account_name)
+
+    # call interface use base64 file
+    result = dark_enhance(token, encode_to_base64('data/dark-enhance-demo.bmp'), '0.95')
+    result_obj = json.loads(result)
+    decode_to_wave_file(result_obj['result'], 'data/dark-enhance-demo-token.bmp')
---
+++ python/image_sdk/super_resolution.py
@@ -0,0 +1,66 @@
+# -*- coding:utf-8 -*-
+
+import sys
+import json
+import image_sdk.ais as ais
+import image_sdk.utils as utils
+import image_sdk.signer as signer
+
+#
+# access image super resolution,post data by token
+#
+def super_resolution(token, image, scale=3, model="ESPCN"):
+    endpoint = utils.get_endpoint(ais.AisService.IMAGE_SERVICE)
+    _url = 'https://%s%s' % (endpoint, ais.ImageURI.SUPER_RESOLUTION)
+
+    if sys.version_info.major >= 3:
+        if image != '':
+            image = image.decode("utf-8")
+
+    _data = {
+        "image": image,
+        "scale": scale,
+        "model": model
+    }
+
+    status_code, resp = utils.request_token(_url, _data, token)
+    if sys.version_info.major < 3:
+        return resp
+    else:
+        return resp.decode('utf-8')
+
+#
+# access image super resolution enhance,post data by sk,sk
+#
+def super_resolution_aksk(_ak, _sk, image, scale=3, model="ESPCN"):
+    endpoint = utils.get_endpoint(ais.AisService.IMAGE_SERVICE)
+    _url = 'https://%s%s' % (endpoint, ais.ImageURI.SUPER_RESOLUTION)
+
+    sig = signer.Signer()
+    sig.AppKey = _ak
+    sig.AppSecret = _sk
+
+    if sys.version_info.major >= 3:
+        if image != '':
+            image = image.decode("utf-8")
+
+    _data = {
+        "image": image,
+        "scale": scale,
+        "model": model
+    }
+
+    kreq = signer.HttpRequest()
+    kreq.scheme = "https"
+    kreq.host = endpoint
+    kreq.uri = "/v1.0/vision/super-resolution"
+    kreq.method = "POST"
+    kreq.headers = {"Content-Type": "application/json"}
+    kreq.body = json.dumps(_data)
+
+    status_code, resp = utils.request_aksk(sig, kreq, _url)
+    if sys.version_info.major < 3:
+        return resp
+    else:
+        return resp.decode('utf-8')
+
