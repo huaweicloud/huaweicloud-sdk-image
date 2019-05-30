67b02a2a8cd1f913423941dcbe258a26a6911fa9
Meekol <942049649@qq.com>
2019-05-30 11:40:29 +0800

    c#版本的内容审核服务sdk迁移

---
+++ c#/src/Models/Ais.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Image.Models
+{
+    public class Ais
+    {
+        // the uri for get token 
+        public static String IAM_TOKEN = "/v3/auth/tokens";
+
+        // the uri for Service image tagging 
+        public static String IMAGE_TAGGING = "/v1.0/image/tagging";
+
+        // the uri for Service asr bgm 
+        public static String ASR_BGM = "/v1.0/bgm/recognition";
+
+        // the uri for Service celebrity recognition
+        public static String CELEBRITY_RECOGNITION = "/v1.0/image/celebrity-recognition";
+
+        // the uri for Service dark enhance
+        public static String DARK_ENHANCE = "/v1.0/vision/dark-enhance";
+
+        // the uri for Service image defog
+        public static String IMAGE_DEFOG = "/v1.0/vision/defog";
+
+        // the uri for Service recapture detect
+        public static String RECAPTURE_DETECT = "/v1.0/image/recapture-detect";
+
+        // the uri for Service super resolution
+        public static String SUPER_RESOLUTION = "/v1.0/vision/super-resolution";
+
+        // retry max times
+        public static int RETRY_MAX_TIMES = 3;
+    }
+}
---
+++ c#/src/Models/ImageService.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Image.Models
+{
+    public class ImageService
+    {
+        private static Dictionary<String, String> endPointsDic = new Dictionary<string, string>();
+
+        static ImageService()
+        {
+            endPointsDic.Add("cn-north-1", "image.cn-north-1.myhuaweicloud.com");
+            endPointsDic.Add("ap-southeast-1", "image.ap-southeast-1.myhuaweicloud.com");
+        }
+
+        public static String AsrBgmToken(String token, String url, String endpoint)
+        {
+            // reuqest data for asr bgm
+            JObject requestBody = new JObject();
+            requestBody.Add("url", url);
+
+            HttpWebRequest request = null;
+            String result = null;
+            String uri = new StringBuilder().Append("https://").Append(endpoint).Append(Ais.ASR_BGM).ToString();
+
+            String serviceName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+            return utils.PostData(request, uri, token, requestBody, result, serviceName);
+
+        }
+
+        public static String ImageTaggingToken(String token, String image, String url, float threshold, String language, int limit, String endpoint)
+        {
+            // reuqest data for image tagging
+            JObject requestBody = new JObject();
+            requestBody.Add("image", image);
+            requestBody.Add("url", url);
+            requestBody.Add("threshold", threshold);
+            requestBody.Add("limit", limit);
+            requestBody.Add("language", language);
+
+            HttpWebRequest request = null;
+            String result = null;
+            String uri = new StringBuilder().Append("https://").Append(endpoint).Append(Ais.IMAGE_TAGGING).ToString();
+
+            String serviceName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+
+            return utils.PostData(request, uri, token, requestBody, result, serviceName);
+
+        }
+
+        public static String CelebrityRecognitionToken(String token, String image, String url, float threshold, String endpoint)
+        {
+            // reuqest data for image tagging
+            JObject requestBody = new JObject();
+            requestBody.Add("image", image);
+            requestBody.Add("url", url);
+            requestBody.Add("threshold", threshold);
+
+            HttpWebRequest request = null;
+            String result = null;
+            String uri = new StringBuilder().Append("https://").Append(endpoint).Append(Ais.CELEBRITY_RECOGNITION).ToString();
+
+            String serviceName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+
+            return utils.PostData(request, uri, token, requestBody, result, serviceName);
+
+        }
+
+        public static String DarkEnhanceToken(String token, String data, float brightness, String endpoint)
+        {
+            // reuqest data for dark enhance
+            JObject requestBody = new JObject();
+            requestBody.Add("image", data);
+            requestBody.Add("brightness", brightness);
+
+            HttpWebRequest request = null;
+            String result = null;
+            String uri = new StringBuilder().Append("https://").Append(endpoint).Append(Ais.DARK_ENHANCE).ToString();
+
+            String serviceName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+
+            return utils.PostData(request, uri, token, requestBody, result, serviceName);
+
+        }
+
+        public static String ImageDefogToken(String token, String data, float gamma, bool natural_look, String endpoint)
+        {
+            // reuqest data for image defog
+            JObject requestBody = new JObject();
+            requestBody.Add("image", data);
+            requestBody.Add("gamma", gamma);
+            requestBody.Add("natural_look", natural_look);
+
+            HttpWebRequest request = null;
+            String result = null;
+            String uri = new StringBuilder().Append("https://").Append(endpoint).Append(Ais.IMAGE_DEFOG).ToString();
+
+            String serviceName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+
+            return utils.PostData(request, uri, token, requestBody, result, serviceName);
+
+        }
+
+        public static String RecaptureDetectToken(String token, String data, String url, float threshold, JArray scene, String endpoint)
+        {
+            // reuqest data for image defog
+            JObject requestBody = new JObject();
+            requestBody.Add("image", data);
+            requestBody.Add("url", url);
+            requestBody.Add("threshold", threshold);
+            requestBody.Add("scene", scene);
+
+            HttpWebRequest request = null;
+            String result = null;
+            String uri = new StringBuilder().Append("https://").Append(endpoint).Append(Ais.RECAPTURE_DETECT).ToString();
+
+            String serviceName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+
+            return utils.PostData(request, uri, token, requestBody, result, serviceName);
+
+        }
+
+        public static String SuperResolutionToken(String token, String data, int scale, String model, String endpoint)
+        {
+            // reuqest data for image defog
+            JObject requestBody = new JObject();
+            requestBody.Add("image", data);
+            requestBody.Add("scale", scale);
+            requestBody.Add("model", model);
+
+            HttpWebRequest request = null;
+            String result = null;
+            String uri = new StringBuilder().Append("https://").Append(endpoint).Append(Ais.SUPER_RESOLUTION).ToString();
+
+            String serviceName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+
+            return utils.PostData(request, uri, token, requestBody, result, serviceName);
+
+        }
+
+        public static String getEndponit(String region)
+        {
+            String endpoint = endPointsDic[region];
+            return endpoint;
+        }
+    }
+}
