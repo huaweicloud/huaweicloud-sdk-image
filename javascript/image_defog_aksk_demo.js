33877c74a2bf0c4a8b5dd563394c95b9e47b79cf
Meekol <942049649@qq.com>
2019-05-31 16:49:01 +0800

    javascript版本图像识别sdk增加cn-north-4的支持，全局iam的修改

--- javascript/image_defog_aksk_demo.js
+++ javascript/image_defog_aksk_demo.js
@@ -4,7 +4,7 @@
 var defog = require("./image_sdk/image_defog");
 var utils = require("./image_sdk/utils");
 
-// 初始化服务的区域信息，目前支持华北-北京一(cn-north-1)、亚太-香港(ap-southeast-1)等区域信息
+// 初始化服务的区域信息，目前支持华北-北京(cn-north-1、cn-north-4)、亚太-香港(ap-southeast-1)等区域信息
 utils.initRegion("cn-north-1");
 
 var app_key = "*************";
