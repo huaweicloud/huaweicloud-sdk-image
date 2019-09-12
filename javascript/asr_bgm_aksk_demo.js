4dff6fda4501fe31d2b4f3deb635ee2a6192c466
Meekol <942049649@qq.com>
2019-09-12 17:12:38 +0800

    javascript版本image服务sdk默认的region更新到cn-north-4

--- javascript/asr_bgm_aksk_demo.js
+++ javascript/asr_bgm_aksk_demo.js
@@ -4,14 +4,14 @@
 var bgm = require("./image_sdk/asr_bgm");
 var utils = require("./image_sdk/utils");
 
-// 初始化服务的区域信息，目前支持华北-北京(cn-north-1、cn-north-4)、亚太-香港(ap-southeast-1)等区域信息
-utils.initRegion("cn-north-1");
+// 初始化服务的区域信息，目前支持华北-北京(cn-north-4)、亚太-香港(ap-southeast-1)等区域信息
+utils.initRegion("cn-north-4");
 
 var app_key = "**************";
 var app_secret = "************";
 
 // obs链接需要和region区域一致，不同的region的obs资源不共享
-obsUrl = "https://obs-test-llg.obs.cn-north-1.myhuaweicloud.com/bgm_recognition";
+obsUrl = "https://obs-image-bj4.obs.cn-north-4.myhuaweicloud.com/bgm_recognition";
 
 bgm.asr_bgm_aksk(app_key, app_secret, obsUrl, function (result) {
     console.log(result);
--- javascript/asr_bgm_token_demo.js
+++ javascript/asr_bgm_token_demo.js
@@ -5,15 +5,15 @@ var bgm = require("./image_sdk/asr_bgm");
 var token = require("./image_sdk/gettoken");
 var utils = require("./image_sdk/utils");
 
-// 初始化服务的区域信息，目前支持华北-北京(cn-north-1、cn-north-4)、亚太-香港(ap-southeast-1)等区域信息
-utils.initRegion("cn-north-1");
+// 初始化服务的区域信息，目前支持华北-北京(cn-north-4)、亚太-香港(ap-southeast-1)等区域信息
+utils.initRegion("cn-north-4");
 
 var username = "*******";        // 配置用户名
 var domain_name = "*******";     // 配置用户名
 var password = "*******";        // 密码
 
 // obs链接需要和region区域一致，不同的region的obs资源不共享
-obsUrl = "https://obs-test-llg.obs.cn-north-1.myhuaweicloud.com/bgm_recognition";
+obsUrl = "https://obs-image-bj4.obs.cn-north-4.myhuaweicloud.com/bgm_recognition";
 token.getToken(username, domain_name, password, function (token) {
 
     bgm.asr_bgm(token, obsUrl, function (result) {
